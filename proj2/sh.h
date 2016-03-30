1b5d586ea53c2c56a753f5befffc9122f4aca824
rkraft <rkraft@udel.edu>
2016-03-30 00:42:29 -0400

    Restored shell to running on solaris machine

--- proj2/sh.h
+++ proj2/sh.h
@@ -12,17 +12,13 @@ int sh( int argc, char **argv, char **envp);
 char *which(char *command, struct pathelement *pathlist);
 char *where(char *command, struct pathelement *pathlist);
 char* cd(char* dest, char* previousDirectory);
-void list ( char *dir );
 struct HistoryNode* addToHistory(struct HistoryNode* tail, char* command);
 void quit_sig_handler(int sig);
 char *SetDir(char **args,int currentPos);
+void list ( char *dir );
 void PrintPrompt();
 void PrintPrecurser(char *arg);
 char **CheckWCChars(char **args);
-
-
-
-
 
 #define PROMPTMAX 32
 #define MAXARGS 10
